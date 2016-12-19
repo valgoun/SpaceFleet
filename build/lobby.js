fc114b93f10d3f154d4f8fdaecd3ec959a691bba
valgoun <valgoun@gmail.com>
2016-12-20 00:17:42 +0100

    Button binding action and basic scene transition

--- build/lobby.js
+++ build/lobby.js
@@ -1,15 +1,55 @@
 var lobby = (function () {
     function lobby() {
-        this.socket = io.connect("http://localhost:8080");
-        $(document).ready(this.Awake);
+        // this.socket = io.connect("http://localhost:8080");
+        $(document).ready(this.Awake.bind(this));
     }
     lobby.prototype.Awake = function () {
         var _this = this;
-        console.log($('#SelectName'));
-        $('#caca').submit(function () {
-            console.log("caca");
-            _this.socket.emit('fleetName', $('#fleetName').val());
-            $('#SelectName').hide();
+        //div initialisation
+        this.NameSelection = $("#SelectName");
+        this.ChoosePanel = $("#ChoosePanel");
+        this.CreationLobby = $("#CreationLobby");
+        this.SelectionLobby = $("#SelectionLobby");
+        this.ActualLobby = $("#Lobby");
+        //Button binding
+        $("#Go").click(function () {
+            console.log("Go");
+            _this.NameSelection.hide();
+            _this.ChoosePanel.show();
+            return false;
+        });
+        $("#Join").click(function () {
+            console.log("Join");
+            _this.ChoosePanel.hide();
+            _this.SelectionLobby.show();
+            return false;
+        });
+        $("#Create").click(function () {
+            console.log("Create");
+            _this.ChoosePanel.hide();
+            _this.CreationLobby.show();
+            return false;
+        });
+        $("#CreateLobby").click(function () {
+            console.log("CreateLobby");
+            _this.CreationLobby.hide();
+            _this.ActualLobby.show();
+            return false;
+        });
+        $("#Connect").click(function () {
+            console.log("Connect");
+            _this.SelectionLobby.hide();
+            _this.ActualLobby.show();
+            return false;
+        });
+        $("#Leave").click(function () {
+            console.log("Leave");
+            _this.ActualLobby.hide();
+            _this.SelectionLobby.show();
+            return false;
+        });
+        $("#Play").click(function () {
+            console.log("Play");
             return false;
         });
     };
--- src/lobby.ts
+++ src/lobby.ts
@@ -1,19 +1,68 @@
 class lobby {
     socket: SocketIOClient.Socket;
+
+    //the different scene of the lobby selection
+    NameSelection: JQuery;
+    ChoosePanel: JQuery;
+    CreationLobby: JQuery;
+    SelectionLobby: JQuery;
+    ActualLobby: JQuery;
+
     constructor() {
-        this.socket = io.connect("http://localhost:8080");
-        $(document).ready(this.Awake);
+        // this.socket = io.connect("http://localhost:8080");
+        $(document).ready(this.Awake.bind(this));
     }
 
     Awake() {
-        console.log($('#SelectName'));
+        //div initialisation
+        this.NameSelection = $("#SelectName");
+        this.ChoosePanel = $("#ChoosePanel");
+        this.CreationLobby = $("#CreationLobby");
+        this.SelectionLobby = $("#SelectionLobby");
+        this.ActualLobby = $("#Lobby");
 
-        $('#caca').submit(() => {
-            console.log("caca");
-            this.socket.emit('fleetName', $('#fleetName').val());
-            $('#SelectName').hide();
+        //Button binding
+        $("#Go").click(() => {
+            console.log("Go");
+            this.NameSelection.hide();
+            this.ChoosePanel.show();
             return false;
         });
+        $("#Join").click(() => {
+            console.log("Join");
+            this.ChoosePanel.hide();
+            this.SelectionLobby.show();
+            return false;
+        });
+        $("#Create").click(() => {
+            console.log("Create");
+            this.ChoosePanel.hide();
+            this.CreationLobby.show();
+            return false;
+        });
+        $("#CreateLobby").click(() => {
+            console.log("CreateLobby");
+            this.CreationLobby.hide();
+            this.ActualLobby.show();
+            return false;
+        });
+        $("#Connect").click(() => {
+            console.log("Connect");
+            this.SelectionLobby.hide();
+            this.ActualLobby.show();
+            return false;
+        });
+        $("#Leave").click(() => {
+            console.log("Leave");
+            this.ActualLobby.hide();
+            this.SelectionLobby.show();
+            return false;
+        });
+        $("#Play").click(() => {
+            console.log("Play");
+            return false;
+        });
+
     }
 }
 
