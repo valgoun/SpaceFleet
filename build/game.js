4c0a86f86742b01c4a5cd6616de32fb1c7aacf47
Keryann Malacchina <keryannm@gmail.com>
2016-12-23 16:13:43 +0000

    Working on MotherShips

--- build/game.js
+++ build/game.js
@@ -1,30 +1,56 @@
-//Phaser = require('phaser');
-///<reference path="../typings/phaser/phaser.d.ts" />
+var screenWidth = 500;
+var screenHeight = 500;
 var shipWidth = 40;
 var shipHeight = 40;
 var shipSpeed = 300;
 var shipRotationSpeed = 300;
 var shipDrag = 70;
 var shipMaxVelocity = 200;
+var motherShipWidth = 40;
+var motherShipHeight = 40;
+var playerMotherShipIndex;
+var motherShipsPosition = [{ x: screenWidth * 0.5, y: screenHeight * 0.8 },
+    { x: screenWidth * 0.5, y: screenHeight * 0.2 },
+    { x: screenWidth * 0.2, y: screenHeight * 0.5 },
+    { x: screenWidth * 0.8, y: screenHeight * 0.5 }];
 var SimpleGame = (function () {
     function SimpleGame() {
-        this.game = new Phaser.Game(1280, 720, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update,
-            createSprite: this.createSprite, shipsMovement: this.shipsMovement, createShip: this.createShip });
+        this.game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update,
+            createSprite: this.createSprite, shipsMovement: this.shipsMovement, createShip: this.createShip,
+            createMotherShip: this.createMotherShip });
     }
     SimpleGame.prototype.preload = function () {
-        this.game.load.image('ship', 'src/assets/Ship.png');
+        this.game.load.image('Ship', 'src/assets/Ship.png');
+        this.game.load.image('MotherShip', 'src/assets/MotherShip.png');
     };
     SimpleGame.prototype.create = function () {
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.ships = [];
+        this.motherShips = [];
+        this.createMotherShip(true);
+        this.createMotherShip(false);
+        console.log("position : " + this.motherShips[playerMotherShipIndex].position);
+        this.motherShips[playerMotherShipIndex].angle = 90;
+        console.log("angle : " + this.motherShips[playerMotherShipIndex].angle);
         this.createShip(0);
-        this.createShip(1);
+        //this.createShip(1);
     };
     SimpleGame.prototype.createShip = function (shipIndex) {
-        this.ships[shipIndex] = this.createSprite('ship', 400, 300, shipHeight, shipWidth);
+        this.ships[shipIndex] = this.createSprite('Ship', 0, 0, shipHeight, shipWidth);
         this.game.physics.arcade.enable(this.ships[shipIndex]);
         this.ships[shipIndex].body.drag.set(shipDrag);
         this.ships[shipIndex].body.maxVelocity.set(shipMaxVelocity);
+    };
+    SimpleGame.prototype.createMotherShip = function (playerMotherShip) {
+        var index = 0;
+        for (var i = 0; i < 4; i++)
+            if (typeof this.motherShips[i] === 'undefined') {
+                index = i;
+                break;
+            }
+        this.motherShips[index] = this.createSprite('MotherShip', motherShipsPosition[index].x, motherShipsPosition[index].y, motherShipWidth, motherShipHeight);
+        if (playerMotherShip)
+            playerMotherShipIndex = index;
     };
     SimpleGame.prototype.createSprite = function (name, x, y, width, height) {
         var sprite = this.game.add.sprite(x, y, name);
@@ -54,14 +80,25 @@ var SimpleGame = (function () {
             }
         }
         if (typeof this.ships[1] !== 'undefined') {
-            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.K)) {
                 this.ships[1].body.angularVelocity = -shipRotationSpeed;
             }
-            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.M)) {
                 this.ships[1].body.angularVelocity = shipRotationSpeed;
             }
             else {
                 this.ships[1].body.angularVelocity = 0;
+            }
+        }
+        if (typeof this.ships[2] !== 'undefined') {
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                this.ships[2].body.angularVelocity = -shipRotationSpeed;
+            }
+            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                this.ships[2].body.angularVelocity = shipRotationSpeed;
+            }
+            else {
+                this.ships[2].body.angularVelocity = 0;
             }
         }
     };
--- src/game.ts
+++ src/game.ts
@@ -1,45 +1,85 @@
-//Phaser = require('phaser');
-///<reference path="../typings/phaser/phaser.d.ts" />
+  const screenWidth = 500;
+  const screenHeight = 500;
+  
+  const shipWidth: number = 40;
+  const shipHeight: number = 40;
 
-  let shipWidth: number = 40;
-  let shipHeight: number = 40;
+  const shipSpeed: number = 300;
+  const shipRotationSpeed = 300;
+  const shipDrag = 70;
+  const shipMaxVelocity = 200;
 
-  let shipSpeed: number = 300;
-  let shipRotationSpeed = 300;
-  let shipDrag = 70;
-  let shipMaxVelocity = 200;
+  const motherShipWidth: number = 40;
+  const motherShipHeight: number = 40;
+  let playerMotherShipIndex: number;
+
+  const motherShipsPosition = 
+  [{x: screenWidth * 0.5, y: screenHeight * 0.8},
+  {x: screenWidth * 0.5, y: screenHeight * 0.2},
+  {x: screenWidth * 0.2, y: screenHeight * 0.5},
+  {x: screenWidth * 0.8, y: screenHeight * 0.5}];
 
 class SimpleGame {
 
     constructor() {
-        this.game = new Phaser.Game(1280, 720, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update, 
-            createSprite: this.createSprite, shipsMovement: this.shipsMovement, createShip: this.createShip});
+        this.game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update, 
+            createSprite: this.createSprite, shipsMovement: this.shipsMovement, createShip: this.createShip, 
+            createMotherShip: this.createMotherShip});
     }
 
     game: Phaser.Game;
     cursors: Phaser.CursorKeys;
 
     ships: Phaser.Sprite[];
+    motherShips: Phaser.Sprite[];
 
     preload() {
-        this.game.load.image('ship', 'src/assets/Ship.png');
+        this.game.load.image('Ship', 'src/assets/Ship.png');
+        this.game.load.image('MotherShip', 'src/assets/MotherShip.png');
     }
 
     create() {
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.ships = [];
+        this.motherShips = [];
+        
+        this.createMotherShip(true);
+        this.createMotherShip(false);
+
+        console.log("position : " + this.motherShips[playerMotherShipIndex].position);
+
+        this.motherShips[playerMotherShipIndex].angle = 90;
+
+        console.log("angle : " + this.motherShips[playerMotherShipIndex].angle);
 
         this.createShip(0);
-        this.createShip(1);
+        //this.createShip(1);
     }
 
     createShip(shipIndex: number)
     {
-        this.ships[shipIndex] = this.createSprite('ship', 400, 300, shipHeight, shipWidth);
+        this.ships[shipIndex] = this.createSprite('Ship', 0, 0, shipHeight, shipWidth);
 
         this.game.physics.arcade.enable(this.ships[shipIndex]);
         this.ships[shipIndex].body.drag.set(shipDrag);
         this.ships[shipIndex].body.maxVelocity.set(shipMaxVelocity);
+    }
+
+    createMotherShip (playerMotherShip: boolean)
+    {
+        let index = 0;
+
+        for(let i = 0; i < 4; i++)
+            if(typeof this.motherShips[i] === 'undefined')
+            {
+                index = i;
+                break;
+            }
+
+        this.motherShips[index] = this.createSprite('MotherShip', motherShipsPosition[index].x, motherShipsPosition[index].y, motherShipWidth, motherShipHeight);
+        
+        if(playerMotherShip)
+            playerMotherShipIndex = index;
     }
 
     createSprite(name: string, x: number, y: number, width: number, height: number) {
@@ -79,14 +119,27 @@ class SimpleGame {
 
         if(typeof this.ships[1] !== 'undefined')
         {   
-            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.K)) {
                 this.ships[1].body.angularVelocity = -shipRotationSpeed;
             }
-            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.M)) {
                 this.ships[1].body.angularVelocity = shipRotationSpeed;
             }
             else {
                 this.ships[1].body.angularVelocity = 0;
+            }
+        }
+
+        if(typeof this.ships[2] !== 'undefined')
+        {   
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                this.ships[2].body.angularVelocity = -shipRotationSpeed;
+            }
+            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                this.ships[2].body.angularVelocity = shipRotationSpeed;
+            }
+            else {
+                this.ships[2].body.angularVelocity = 0;
             }
         }
     }
