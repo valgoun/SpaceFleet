636a53c20f525663d2c034ebab36f79a65a52302
Keryann Malacchina <keryannm@gmail.com>
2016-12-25 22:22:55 +0100

    Colliders and Collisions Done

--- build/game.js
+++ build/game.js
@@ -1,5 +1,7 @@
-var screenWidth = 700;
-var screenHeight = 700;
+//Screen Variables
+var screenWidth = 1024;
+var screenHeight = 576;
+//Ships Variables
 var shipWidth = 40;
 var shipHeight = 40;
 var shipSpeed = 300;
@@ -7,6 +9,8 @@ var shipRotationSpeed = 300;
 var shipDrag = 70;
 var shipMaxVelocity = 200;
 var shipsSpawnGap = 60;
+var shipsColliderRadius = 15;
+//MotherShips Variables
 var motherShipWidth = 240;
 var motherShipHeight = 120;
 var playerMotherShipIndex;
@@ -15,26 +19,37 @@ var motherShipsPosition = [{ x: screenWidth * 0.5, y: screenHeight * 0.8 },
     { x: screenWidth * 0.2, y: screenHeight * 0.5 },
     { x: screenWidth * 0.8, y: screenHeight * 0.5 }];
 var motherShipsAngles = [0, 180, 90, 270];
+var motherShipsWidthCollider = 0.9;
+var motherShipsHeightCollider = 0.5;
 var SimpleGame = (function () {
     function SimpleGame() {
+        //Create Phaser Game With All Functions Needed
         this.game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'content', {
-            preload: this.preload, create: this.create, update: this.update,
+            preload: this.preload, create: this.create, update: this.update, render: this.render,
             createSprite: this.createSprite, shipsMovement: this.shipsMovement, createShip: this.createShip,
-            createMotherShip: this.createMotherShip, getShipSpawnPosition: this.getShipSpawnPosition
+            createMotherShip: this.createMotherShip, getShipSpawnPosition: this.getShipSpawnPosition,
+            collisions: this.collisions, destroyShip: this.destroyShip
         });
     }
     SimpleGame.prototype.preload = function () {
+        //Load Images For Sprites
         this.game.load.image('Ship', 'src/assets/Ship.png');
         this.game.load.image('MotherShip', 'src/assets/MotherShip.png');
     };
     SimpleGame.prototype.create = function () {
+        //Initialize Arrays
         this.ships = [];
         for (var i = 0; i < 4; i++) {
             this.ships[i] = [];
         }
         this.motherShips = [];
-        this.backgroundGroup = this.game.add.group();
-        this.shipsGroup = this.game.add.group();
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        //Initialize Groups        
+        this.playerMotherShipGroup = this.game.add.group();
+        this.enemiesMotherShipsGroup = this.game.add.group();
+        this.playerShipsGroup = this.game.add.group();
+        this.enemiesShipsGroup = this.game.add.group();
+        //Create MotherShips and Ships        
         this.createMotherShip(true);
         this.createMotherShip(false);
         this.createMotherShip(false);
@@ -54,6 +69,7 @@ var SimpleGame = (function () {
     };
     SimpleGame.prototype.createMotherShip = function (playerMotherShip) {
         var index = 0;
+        //Check Which Slot is Free
         for (var i = 0; i < 4; i++)
             if (typeof this.motherShips[i] === 'undefined') {
                 index = i;
@@ -61,19 +77,42 @@ var SimpleGame = (function () {
             }
         this.motherShips[index] = this.createSprite('MotherShip', motherShipsPosition[index], motherShipWidth, motherShipHeight);
         this.motherShips[index].angle = motherShipsAngles[index];
-        this.backgroundGroup.add(this.motherShips[index]);
-        if (playerMotherShip)
+        //Set Physics Settings        
+        this.game.physics.arcade.enable(this.motherShips[index]);
+        this.motherShips[index].body.collideWorldBounds = true;
+        this.motherShips[index].body.immovable = true;
+        //Set Collider Bounds
+        if (index < 2) {
+            this.motherShips[index].body.setSize(944 * motherShipsWidthCollider, 447 * motherShipsHeightCollider, 944 * (1 - motherShipsWidthCollider) * 0.5, 447 * (1 - motherShipsHeightCollider) * 0.5);
+        }
+        else {
+            this.motherShips[index].body.setSize(447 * motherShipsHeightCollider, (944 * motherShipsWidthCollider) - 80, 944 * 0.381, (-447 + 80) * 0.5);
+        }
+        //Set Group        
+        if (playerMotherShip) {
             playerMotherShipIndex = index;
+            this.playerMotherShipGroup.add(this.motherShips[index]);
+        }
+        else
+            this.enemiesMotherShipsGroup.add(this.motherShips[index]);
     };
     SimpleGame.prototype.createShip = function (motherShipIndex, shipIndex) {
-        this.ships[motherShipIndex][shipIndex] = this.createSprite('Ship', this.getShipSpawnPosition(shipIndex, motherShipIndex), shipHeight, shipWidth);
+        this.ships[motherShipIndex][shipIndex] = this.createSprite('Ship', this.getShipSpawnPosition(motherShipIndex, shipIndex), shipHeight, shipWidth);
+        //Set Physics Settings        
         this.game.physics.arcade.enable(this.ships[motherShipIndex][shipIndex]);
         this.ships[motherShipIndex][shipIndex].body.drag.set(shipDrag);
         this.ships[motherShipIndex][shipIndex].body.maxVelocity.set(shipMaxVelocity);
+        //Set Ship Spawn Angle
         this.ships[motherShipIndex][shipIndex].angle = this.motherShips[motherShipIndex].angle - 90;
-        this.shipsGroup.add(this.ships[motherShipIndex][shipIndex]);
+        this.ships[motherShipIndex][shipIndex].body.setCircle(shipsColliderRadius, 720 * 0.5, 713 * 0.5);
+        //Set Ship Group
+        if (motherShipIndex === playerMotherShipIndex)
+            this.playerShipsGroup.add(this.ships[motherShipIndex][shipIndex]);
+        else
+            this.enemiesShipsGroup.add(this.ships[motherShipIndex][shipIndex]);
     };
-    SimpleGame.prototype.getShipSpawnPosition = function (shipIndex, motherShipIndex) {
+    //Get Ship Spawn Position From MotherShip    
+    SimpleGame.prototype.getShipSpawnPosition = function (motherShipIndex, shipIndex) {
         var x = 0;
         var y = 0;
         if (motherShipIndex < 2) {
@@ -106,6 +145,7 @@ var SimpleGame = (function () {
         }
         return { x: x, y: y };
     };
+    //General Method to create Sprite    
     SimpleGame.prototype.createSprite = function (name, position, width, height) {
         var sprite = this.game.add.sprite(position.x, position.y, name);
         sprite.width = width;
@@ -115,14 +155,22 @@ var SimpleGame = (function () {
     };
     SimpleGame.prototype.update = function () {
         this.shipsMovement();
-        this.game.world.bringToTop(this.shipsGroup);
+        this.collisions();
+        this.game.world.bringToTop(this.playerShipsGroup);
+        this.game.world.bringToTop(this.enemiesShipsGroup);
     };
+    //Movement of 3 players' ships    
     SimpleGame.prototype.shipsMovement = function () {
         for (var i = 0; i < this.ships[playerMotherShipIndex].length; i++)
             if (typeof this.ships[playerMotherShipIndex][i] !== 'undefined') {
                 this.game.physics.arcade.accelerationFromRotation(this.ships[playerMotherShipIndex][i].rotation, shipSpeed, this.ships[playerMotherShipIndex][i].body.acceleration);
-                this.game.world.wrap(this.ships[playerMotherShipIndex][i], 16);
             }
+        //Wrap for all ships
+        for (var i = 0; i < this.ships.length; i++) {
+            for (var j = 0; j < this.ships[i].length; j++)
+                if (typeof this.ships[i][j] !== 'undefined')
+                    this.game.world.wrap(this.ships[i][j], 16);
+        }
         if (typeof this.ships[playerMotherShipIndex][0] !== 'undefined') {
             if (this.game.input.keyboard.isDown(Phaser.Keyboard.Q)) {
                 this.ships[playerMotherShipIndex][0].body.angularVelocity = -shipRotationSpeed;
@@ -155,6 +203,22 @@ var SimpleGame = (function () {
             else {
                 this.ships[playerMotherShipIndex][2].body.angularVelocity = 0;
             }
+        }
+    };
+    SimpleGame.prototype.collisions = function () {
+        this.game.physics.arcade.collide(this.playerShipsGroup, this.enemiesShipsGroup);
+        this.game.physics.arcade.collide(this.playerShipsGroup, this.enemiesMotherShipsGroup, this.destroyShip.bind(this));
+    };
+    SimpleGame.prototype.destroyShip = function (motherShipIndex, shipIndex) {
+        console.log("Dead : " + motherShipIndex + shipIndex);
+    };
+    SimpleGame.prototype.render = function () {
+        //Debug Colliders
+        for (var i = 0; i < this.ships.length; i++) {
+            //this.game.debug.body(this.motherShips[i]);
+            for (var j = 0; j < this.ships[i].length; j++)
+                if (typeof this.ships[i][j] !== 'undefined') {
+                }
         }
     };
     return SimpleGame;
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,8 @@
-const screenWidth = 700;
-const screenHeight = 700;
+//Screen Variables
+const screenWidth = 1024;
+const screenHeight = 576;
 
+//Ships Variables
 const shipWidth: number = 40;
 const shipHeight: number = 40;
 
@@ -11,6 +13,9 @@ const shipMaxVelocity = 200;
 
 const shipsSpawnGap = 60;
 
+const shipsColliderRadius = 15;
+
+//MotherShips Variables
 const motherShipWidth: number = 240;
 const motherShipHeight: number = 120;
 let playerMotherShipIndex: number;
@@ -23,13 +28,18 @@ const motherShipsPosition =
 
 const motherShipsAngles = [0, 180, 90, 270];
 
+const motherShipsWidthCollider = 0.9;
+const motherShipsHeightCollider = 0.5;
+
 class SimpleGame {
 
     constructor() {
+        //Create Phaser Game With All Functions Needed
         this.game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO, 'content', {
-            preload: this.preload, create: this.create, update: this.update,
+            preload: this.preload, create: this.create, update: this.update, render: this.render,
             createSprite: this.createSprite, shipsMovement: this.shipsMovement, createShip: this.createShip,
-            createMotherShip: this.createMotherShip, getShipSpawnPosition: this.getShipSpawnPosition
+            createMotherShip: this.createMotherShip, getShipSpawnPosition: this.getShipSpawnPosition,
+            collisions: this.collisions, destroyShip: this.destroyShip
         });
     }
 
@@ -38,25 +48,35 @@ class SimpleGame {
     ships: Phaser.Sprite[][];
     motherShips: Phaser.Sprite[];
 
-    backgroundGroup: Phaser.Group;
-    shipsGroup: Phaser.Group;
+    //Groups For Sprite Rendering Sort and Collision Layer    
+    playerMotherShipGroup: Phaser.Group;
+    enemiesMotherShipsGroup: Phaser.Group;
+    playerShipsGroup: Phaser.Group;
+    enemiesShipsGroup: Phaser.Group;
 
     preload() {
+        //Load Images For Sprites
         this.game.load.image('Ship', 'src/assets/Ship.png');
         this.game.load.image('MotherShip', 'src/assets/MotherShip.png');
     }
 
     create() {
+        //Initialize Arrays
         this.ships = [];
         for (let i = 0; i < 4; i++) {
             this.ships[i] = [];
         }
-
         this.motherShips = [];
 
-        this.backgroundGroup = this.game.add.group();
-        this.shipsGroup = this.game.add.group();
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        //Initialize Groups        
+        this.playerMotherShipGroup = this.game.add.group();
+        this.enemiesMotherShipsGroup = this.game.add.group();
+        this.playerShipsGroup = this.game.add.group();
+        this.enemiesShipsGroup = this.game.add.group();
+
+        //Create MotherShips and Ships        
         this.createMotherShip(true);
         this.createMotherShip(false);
         this.createMotherShip(false);
@@ -82,6 +102,7 @@ class SimpleGame {
     createMotherShip(playerMotherShip: boolean) {
         let index = 0;
 
+        //Check Which Slot is Free
         for (let i = 0; i < 4; i++)
             if (typeof this.motherShips[i] === 'undefined') {
                 index = i;
@@ -91,25 +112,55 @@ class SimpleGame {
         this.motherShips[index] = this.createSprite('MotherShip', motherShipsPosition[index], motherShipWidth, motherShipHeight);
         this.motherShips[index].angle = motherShipsAngles[index];
 
-        this.backgroundGroup.add(this.motherShips[index]);
+        //Set Physics Settings        
+        this.game.physics.arcade.enable(this.motherShips[index]);
+        this.motherShips[index].body.collideWorldBounds = true;
+        this.motherShips[index].body.immovable = true;
 
-        if (playerMotherShip)
+        //Set Collider Bounds
+        if (index < 2) {
+            this.motherShips[index].body.setSize(944 * motherShipsWidthCollider, 447 * motherShipsHeightCollider,
+                944 * (1 - motherShipsWidthCollider) * 0.5, 447 * (1 - motherShipsHeightCollider) * 0.5);
+        }
+        else {
+            this.motherShips[index].body.setSize(447 * motherShipsHeightCollider, (944 * motherShipsWidthCollider) - 80,
+                944 * 0.381, (-447 + 80) * 0.5);
+        }
+
+
+        //Set Group        
+        if (playerMotherShip) {
             playerMotherShipIndex = index;
+            this.playerMotherShipGroup.add(this.motherShips[index]);
+        }
+        else
+            this.enemiesMotherShipsGroup.add(this.motherShips[index]);
+
     }
 
     createShip(motherShipIndex: number, shipIndex: number) {
-        this.ships[motherShipIndex][shipIndex] = this.createSprite('Ship', this.getShipSpawnPosition(shipIndex, motherShipIndex), shipHeight, shipWidth);
+        this.ships[motherShipIndex][shipIndex] = this.createSprite('Ship', this.getShipSpawnPosition(motherShipIndex, shipIndex), shipHeight, shipWidth);
 
+        //Set Physics Settings        
         this.game.physics.arcade.enable(this.ships[motherShipIndex][shipIndex]);
         this.ships[motherShipIndex][shipIndex].body.drag.set(shipDrag);
         this.ships[motherShipIndex][shipIndex].body.maxVelocity.set(shipMaxVelocity);
 
+        //Set Ship Spawn Angle
         this.ships[motherShipIndex][shipIndex].angle = this.motherShips[motherShipIndex].angle - 90;
 
-        this.shipsGroup.add(this.ships[motherShipIndex][shipIndex]);
+        this.ships[motherShipIndex][shipIndex].body.setCircle(shipsColliderRadius, 720 * 0.5, 713 * 0.5);
+
+
+        //Set Ship Group
+        if (motherShipIndex === playerMotherShipIndex)
+            this.playerShipsGroup.add(this.ships[motherShipIndex][shipIndex]);
+        else
+            this.enemiesShipsGroup.add(this.ships[motherShipIndex][shipIndex]);
     }
 
-    getShipSpawnPosition(shipIndex: number, motherShipIndex: number) {
+    //Get Ship Spawn Position From MotherShip    
+    getShipSpawnPosition(motherShipIndex: number, shipIndex: number) {
         let x = 0;
         let y = 0;
 
@@ -148,6 +199,7 @@ class SimpleGame {
         return { x: x, y: y };
     }
 
+    //General Method to create Sprite    
     createSprite(name: string, position: { x: number; y: number }, width: number, height: number) {
         let sprite = this.game.add.sprite(position.x, position.y, name);
         sprite.width = width;
@@ -159,16 +211,26 @@ class SimpleGame {
 
     update() {
         this.shipsMovement();
+        this.collisions();
 
-        this.game.world.bringToTop(this.shipsGroup);
+        this.game.world.bringToTop(this.playerShipsGroup);
+        this.game.world.bringToTop(this.enemiesShipsGroup);
     }
 
+    //Movement of 3 players' ships    
     shipsMovement() {
         for (let i = 0; i < this.ships[playerMotherShipIndex].length; i++)
             if (typeof this.ships[playerMotherShipIndex][i] !== 'undefined') {
                 this.game.physics.arcade.accelerationFromRotation(this.ships[playerMotherShipIndex][i].rotation, shipSpeed, this.ships[playerMotherShipIndex][i].body.acceleration);
-                this.game.world.wrap(this.ships[playerMotherShipIndex][i], 16);
+                //this.game.world.wrap(this.ships[playerMotherShipIndex][i], 16);
             }
+
+        //Wrap for all ships
+        for (let i = 0; i < this.ships.length; i++) {
+            for (let j = 0; j < this.ships[i].length; j++)
+                if (typeof this.ships[i][j] !== 'undefined')
+                    this.game.world.wrap(this.ships[i][j], 16);
+        }
 
         if (typeof this.ships[playerMotherShipIndex][0] !== 'undefined') {
             if (this.game.input.keyboard.isDown(Phaser.Keyboard.Q)) {
@@ -207,6 +269,27 @@ class SimpleGame {
         }
     }
 
+    collisions() {
+        this.game.physics.arcade.collide(this.playerShipsGroup, this.enemiesShipsGroup);
+        this.game.physics.arcade.collide(this.playerShipsGroup, this.enemiesMotherShipsGroup, this.destroyShip.bind(this));
+    }
+
+    destroyShip(motherShipIndex: number, shipIndex: number) {
+        console.log("Dead : " + motherShipIndex + shipIndex);
+    }
+
+    render() {
+        //Debug Colliders
+        for (let i = 0; i < this.ships.length; i++) {
+            //this.game.debug.body(this.motherShips[i]);
+
+            for (let j = 0; j < this.ships[i].length; j++)
+                if (typeof this.ships[i][j] !== 'undefined') {
+                    //this.game.debug.body(this.ships[i][j]);
+                }
+        }
+
+    }
 }
 
 window.onload = () => {
